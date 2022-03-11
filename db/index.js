0c42b76511ac0378642d31f3060b9668c259f178
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-03-11 13:54:41 +0330

    update pool

--- db/index.js
+++ db/index.js
@@ -13,9 +13,7 @@ const connectionString = `postgresql://${
 // Create new pool
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DATABASE_URL ? isProduction : false,
 });
 
 // Pool error handling
